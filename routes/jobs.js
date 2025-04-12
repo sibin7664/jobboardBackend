8d98b9328b310213c543e20325845a6d35513c4d
sibin7664 <sibinkavungal2@gmail.com>
2025-04-12 20:04:24 +0530

    err in

--- routes/jobs.js
+++ routes/jobs.js
@@ -12,16 +12,5 @@ const router = express.Router();
 router.route("/").post(createJob).get(getAllJobs);       // Get all jobs
 router.get('/:id', getJobById);    // Get job by ID
 
-// GET /jobs - Get all jobs
-router.get('/', async (req, res) => {
-    try {
-        const jobs = await Job.find(); // You can also sort or limit if needed
-        res.status(200).json(jobs);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error fetching jobs' });
-    }
-});
-
 
 export default router;
